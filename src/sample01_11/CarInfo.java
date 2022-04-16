dbce7063527b89f6667e2f890be381dd6f78bfe6
DESKTOP-OJJHRKF\kimch <skqmsskql@naver.com>
2022-04-16 16:17:11 +0900

    no message

---
+++ src/sample01_11/CarInfo.java
@@ -0,0 +1,24 @@
+package sample01_11;
+
+public class CarInfo {
+
+    public CarInfo() {
+
+    }
+
+    public void CheckCarType() {
+        System.out.println("차종 : 경차");
+    }
+
+    public void CheckCarColor() {
+        System.out.println("차 색깔 : 검정, 하양");
+    }
+
+    public void CheckCarPrice() {
+        System.out.println("가격 : 1175 ~ 1520만원");
+    }
+
+    public void CheckFuel() {
+        
+    }
+}
